dcd14a9b253f61ca42a9442ac2dfebc7e6baa7bc
Gideon Ogongo <ogjr80@gmail.com>
2017-07-28 08:33:12 +0200

    futher changed the routes require to conform to standard

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 var express = require('express'); 
 require('./services/passport'); 
-var authRoutes = require('./routes/authRoutes'); 
+
 
 var app = express(); 
-authRoutes(app); 
+require('./routes/authRoutes')(app); 
 
 app.listen(3000, ()=> {
     console.log('server now running on port http://localhost:3000'); 
